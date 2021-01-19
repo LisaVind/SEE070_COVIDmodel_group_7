7a58e3d394de7f82f8cfb019782b15ebebdde64c
LisaVind <LisaVind@users.noreply.github.com>
2021-01-19 15:29:58 +0100

    Update Covid_modell_1.py
    Modell med vaccin

--- Covid_modell_1.py
+++ Covid_modell_1.py
@@ -8,9 +8,9 @@ Created on Wed Dec 16 16:57:20 2020
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-beta = 1.1
+beta = 0.5
 
-gamma = 0.1
+gamma = 0.2
 
 #R0 = initiala värdet på smittspridningen.
 #Beta = antal som smittas per infekterad och per tid (beror på virusets egenskaper samt hur vi beter oss)
@@ -30,8 +30,13 @@ b = 0.25
 #R – tillfrisknade, eller möjligen också döda personer, vi måste utgå från data som finns? Sedan lägga till en faktor I * gamma
 
 R = 350000
+
+V = 20000
+#Antalet vaccinerade
  
-S = N - R - I
+S = N - R - I - V
+
+
 
 #S – de som är mottagliga för infektion 
 
@@ -42,14 +47,25 @@ lista_R = []
 lista_Rt = []
 lista_S = []
 lista_I = []
+lista_V = []
 
 
-for t in range(0,60):
-    R += I * gamma
+for t in range(0,100):
+    V += 2000
+    
+    if V >= 10 * 10**(6):
+        V = 10 * 10**(6)
+    
+    lista_V.append(V)
+    
+    R += I * gamma + V
     lista_R.append(R)
     
+    if R >= 10 * 10**(6):
+        R = 10 * 10**(6)
     
-    S += (-(beta*I*S))/N 
+    
+    S += -(beta)* I * (S/N)
     lista_S.append(S)
     
     I += (beta * I * S)/N - (I * gamma)
@@ -67,11 +83,17 @@ for t in range(0,60):
 plt.plot(lista_S, 'blue')
 plt.plot(lista_I, 'red')
 plt.plot(lista_R, 'g')
+plt.plot(lista_V, 'purple')
+
+print(lista_V)
+print(lista_R)
+
 
 pop_a = mpatches.Patch(color='blue', label='Mottagliga för infektion')
 pop_b = mpatches.Patch(color='red', label='Infekterade')
 pop_c =  mpatches.Patch(color='green', label='Tillfrisknade')
+pop_d =  mpatches.Patch(color='purple', label='Vaccinerade')
 
-plt.legend(handles=[pop_a,pop_b,pop_c])
+plt.legend(handles=[pop_a,pop_b,pop_c, pop_d])
 
 
